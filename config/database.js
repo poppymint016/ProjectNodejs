b8c555e0b2fe022594d77b68e81d68e7aa69f6ac
poppymint016 <phannita016@gmail.com>
2024-01-09 16:52:18 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const { MONGO_URI } = process.env;
+console.log("MONGO_URI:", MONGO_URI);
 
 exports.connect = () => {
+
+    if (!MONGO_URI) {
+        console.error('MONGO_URI is not defined in the environment variables.');
+        process.exit(1);
+      }
+
     mongoose.connect(MONGO_URI, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         
     }).then(() => {
         console.log("Successfully connected to the database");
